c0d0e224886a769177e89d563663c362aa15c160
hamidjabal03 <hamidjabal03@gmail.com>
2026-05-04 02:05:49 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore, collection, doc, onSnapshot, addDoc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getFirestore, collection, doc, onSnapshot, addDoc, setDoc, getDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2xfDX5PU_nP5TEPSYFL6xmBcS0wZrj_c",
@@ -8,27 +8,25 @@ const firebaseConfig = {
   projectId: "ritus-street-inventory",
   storageBucket: "ritus-street-inventory.firebasestorage.app",
   messagingSenderId: "249697895281",
-  appId: "1:249697895281:web:c44bccdd16958c9d28ecd0",
-  measurementId: "G-MMTVPVB24E"
+  appId: "1:249697895281:web:c44bccdd16958c9d28ecd0"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Sediakan data untuk App.js
+// Simpan ke window agar App.js bisa mengaksesnya
 window.RitusDB = { 
-    db, auth, collection, doc, onSnapshot, addDoc, setDoc, getDoc, 
+    db, auth, collection, doc, onSnapshot, addDoc, setDoc, getDoc, query, orderBy,
     signInWithEmailAndPassword, signOut, onAuthStateChanged 
 };
 
-// Pastikan loader hilang hanya setelah data siap
 window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     if (loader) {
         setTimeout(() => {
-            loader.classList.add('fade-out');
-            setTimeout(() => loader.remove(), 400);
+            loader.style.opacity = '0';
+            setTimeout(() => loader.remove(), 500);
         }, 1500);
     }
 });
